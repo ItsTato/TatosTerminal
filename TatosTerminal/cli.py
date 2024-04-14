1a3f9bf0d3a9a807c1d9c699167d76514c65abb4
ItsTato <78940659+ItsTato@users.noreply.github.com>
2024-04-14 17:44:59 -0600

    smush argument

--- TatosTerminal/cli.py
+++ TatosTerminal/cli.py
@@ -1,6 +1,12 @@
+from sys import argv
+
 from .TTSession import Terminal
 
 def main() -> None:
-	Terminal(__file__)
+	smush:bool = False
+	if len(argv) > 1:
+		if "--smush" in argv:
+			smush = True
+	Terminal(__file__,smush)
 	print("\n\nTatosTerminal session was closed.")
 	return
