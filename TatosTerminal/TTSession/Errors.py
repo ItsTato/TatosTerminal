ee1794ff33b2e639b3497bf34b162b1cb88292e7
ItsTato <78940659+ItsTato@users.noreply.github.com>
2024-04-16 01:19:15 -0600

    New error

---
+++ TatosTerminal/TTSession/Errors.py
@@ -0,0 +1,8 @@
+class IncompleteInstallation(Exception):
+	def __init__(self,message:str) -> None:
+		super().__init__(message)
+		self.__message:str = message
+		return
+	@property
+	def message(self) -> str:
+		return self.__message
